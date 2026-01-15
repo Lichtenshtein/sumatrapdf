dncodeai/DNC_sumatrapdf_dev1

feat: render paired pages simultaneously

src/RenderCache.cpp
@@ -413,6 +413,35 @@ void RenderCache::RequestRendering(DisplayModel* dm, int pageNo) {
     }
 }
 
+void RenderCache::RenderSync(DisplayModel* dm, int pageNo) {
+    CrashIf(!dm);
+    if (!dm || dm->dontRenderFlag) {
+        return;
+    }
+    TilePosition tile(0, 0, 0);
+    int rotation = NormalizeRotation(dm->GetRotation());
+    float zoom = dm->GetZoomReal(pageNo);
+
+    if (Exists(dm, pageNo, rotation, zoom, &tile)) {
+        return;
+    }
+
+    PageRenderRequest req{};
+    req.dm = dm;
+    req.pageNo = pageNo;
+    req.rotation = rotation;
+    req.zoom = zoom;
+    req.tile = tile;
+    req.pageRect = GetTileRectUser(dm->GetEngine(), pageNo, rotation, zoom, tile);
+
+    RenderPageArgs args(pageNo, zoom, rotation, &req.pageRect);
+    RenderedBitmap* bmp = dm->GetEngine()->RenderPage(args);
+    if (bmp && !dm->GetEngine()->IsImageCollection()) {
+        UpdateBitmapColors(bmp->GetBitmap(), textColor, backgroundColor);
+    }
+    Add(req, bmp);
+}
+
 /* Render a bitmap for page <pageNo> in <dm>. */
 void RenderCache::RequestRendering(DisplayModel* dm, int pageNo, TilePosition tile, bool clearQueueForPage) {
     logf("RenderCache::RequestRendering(): pageNo %d\n", pageNo);

Lichtenshtein/sumatrapdf

src/RenderCache.cpp
@@ -435,6 +435,35 @@ void RenderCache::RequestRendering(DisplayModel* dm, int pageNo) {
     }
 }
 
+void RenderCache::RenderSync(DisplayModel* dm, int pageNo) {
+    ReportIf(!dm);
+    if (!dm || dm->pauseRendering) {
+        return;
+    }
+    TilePosition tile(0, 0, 0);
+    int rotation = NormalizeRotation(dm->GetRotation());
+    float zoom = dm->GetZoomReal(pageNo);
+
+    if (Exists(dm, pageNo, rotation, zoom, &tile)) {
+        return;
+    }
+
+    PageRenderRequest req{};
+    req.dm = dm;
+    req.pageNo = pageNo;
+    req.rotation = rotation;
+    req.zoom = zoom;
+    req.tile = tile;
+    req.pageRect = GetTileRectUser(dm->GetEngine(), pageNo, rotation, zoom, tile);
+
+    RenderPageArgs args(pageNo, zoom, rotation, &req.pageRect);
+    RenderedBitmap* bmp = dm->GetEngine()->RenderPage(args);
+    if (bmp && !dm->GetEngine()->IsImageCollection()) {
+        UpdateBitmapColors(bmp->GetBitmap(), textColor, backgroundColor);
+    }
+    Add(req, bmp);
+}
+
 /* Render a bitmap for page <pageNo> in <dm>. */
 void RenderCache::RequestRendering(DisplayModel* dm, int pageNo, TilePosition tile, bool clearQueueForPage) {
     logvf("RenderCache::RequestRendering: pageNo %d\n", pageNo);